a31ad86669f92d07a97a4ad82e3f75de1175f427
angieleutard <leutardangie@MacBook-Pro-de-leutard.local>
2024-02-08 15:52:02 +0100

    added filters list

--- filters.js
+++ filters.js
@@ -8,47 +8,73 @@ if (total_recipes_value == 1) {
     total_recipes.innerHTML = `${total_recipes_value} recette`;
 }
 
-// Filters
+// Remove duplicates
+
+function removeDuplicates(data) {
+    return data.filter((value, index) => data.indexOf(value) === index);
+}
+
+// Arrays for filters
+
+let ig_tableau = new Array();
+let reduce_ig_tableau = new Array();
+
+let ap_tableau = new Array();
+let reduce_ap_tableau = new Array();
+
+let us_tableau = new Array();
+let reduce_us_tableau = new Array();
+
+// Filters 
+
 recipes.map((recipe) => {
 
     // Ingredients
-    let ing_list = document.querySelector(".ingredient_select");
     let ingredients = recipe.ingredients;
 
     ingredients.map((ingredient) => {
-
-        let ingredient_li = document.createElement("li");
-        ingredient_li.classList.add("link_container");
-        ingredient_li.innerHTML = ingredient.ingredient;
-
-        ing_list.appendChild(ingredient_li);
+        ig_tableau.push(ingredient.ingredient);
+        reduce_ig_tableau = removeDuplicates(ig_tableau);
     })
 
     // Appareils
-
-    let app_list = document.querySelector(".appliance_select");
     let appliances = recipe.appliance;
 
-    let appliance_li = document.createElement("li");
-    appliance_li.classList.add("link_container");
-    appliance_li.innerHTML = appliances;
-
-    app_list.appendChild(appliance_li);
+    ap_tableau.push(appliances);
+    reduce_ap_tableau = removeDuplicates(ap_tableau);
 
     // Ustensiles
-    let ust_list = document.querySelector(".ustensil_select");
     let ustensils = recipe.ustensils;
 
     ustensils.map((ustensil) => {
-
-        let ustensil_li = document.createElement("li");
-        ustensil_li.classList.add("link_container");
-        ustensil_li.innerHTML = ustensil;
-
-        ust_list.appendChild(ustensil_li);
+        us_tableau.push(ustensil);
+        reduce_us_tableau =removeDuplicates(us_tableau);
     })
 
 })
-    
+
+let ing_list = document.querySelector(".ingredient_select");
+let app_list = document.querySelector(".appliance_select");
+let ust_list = document.querySelector(".ustensil_select");
 
 
+reduce_ig_tableau.forEach((e) => {
+    let ingredient_li = document.createElement("li");
+    ingredient_li.classList.add("link_container");
+    ingredient_li.innerHTML = e;
+    ing_list.appendChild(ingredient_li);
+});
+
+reduce_ap_tableau.forEach((e) => {
+    let appliance_li = document.createElement("li");
+    appliance_li.classList.add("link_container");
+    appliance_li.innerHTML = e;
+    app_list.appendChild(appliance_li);
+});
+
+reduce_us_tableau.forEach((e) => {
+    let ustensil_li = document.createElement("li");
+    ustensil_li.classList.add("link_container");
+    ustensil_li.innerHTML = e;
+    ust_list.appendChild(ustensil_li);
+});
